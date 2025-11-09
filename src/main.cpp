405f1d7b88a23bf9df5ae48e09392b0dfe7d98a3
Dominika Bomba <146015989+DominikaBomba@users.noreply.github.com>
2025-11-09 14:46:46 +0100

    Drawing a particle,
    - Class particle added
    - Shaderss (VARTEX, FRAGMENT)
    - Inputs for mass and Bz

--- src/main.cpp
+++ src/main.cpp
@@ -4,40 +4,65 @@
 #include "imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
+#include "Particle.h"
 
-using namespace std;
 #pragma once
 #include <glm/glm.hpp> // dla pozycji
 #include <glad/glad.h>
+// Funkcja pomocnicza do kompilacji shaderÃ³w
+
+using namespace std;
+
+//MuszÄ… byÄ‡ Shader7
+GLuint CompileShader(GLenum type, const char* source) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, nullptr);
+    glCompileShader(shader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        cerr << "Shader compilation failed:\n" << infoLog << endl;
+    }
+    return shader;
+}
 
 
-//kiedy zmieniamy rozmiar okna, funkcja jest wywo³ywana 
-//(mówi openGL która czêœæ okna bêdzie renderowana)
+
+//kiedy zmieniamy rozmiar okna, funkcja jest wywoï¿½ywana 
+//(mï¿½wi openGL ktï¿½ra czï¿½ï¿½ okna bï¿½dzie renderowana)
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
 
-int main() 
+int main()
 {
-    //glfwInit uruchamia nam t¹ biblioreke GLFW - odpowiada za wygenerowanie okna
+    Particle particle({ 0.0, 0.0 }, { 1.0, 0.0 }, 1.0, 1.0);
+    double Bz = 1.0;
+    double dt = 0.001;
+    double masa = 0.1;
+    bool simulate = false;
+    //glfwInit uruchamia nam tï¿½ biblioreke GLFW - odpowiada za wygenerowanie okna
     if (!glfwInit()) {
-        //cerr to takie cout tylko dla blêdów
-        cerr << "Inicjacja GLFW siê nie uda³a\n";
+        //cerr to takie cout tylko dla blï¿½dï¿½w
+        cerr << "Inicjacja GLFW siï¿½ nie udaï¿½a\n";
         return -1;
     }
 
-    //ustawiamy dobr¹ wersjê GLFW
+    //ustawiamy dobrÄ… wersje GLFW
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     //funkcja glfwCreateWindow tworzy nam okno 
-    //glfwTerminate "sprz¹ta po glfw" zamyka wszsystkie okna itd
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Tu damy tytu³:D", nullptr, nullptr);
+    //glfwTerminate "sprzata po glfw" zamyka wszsystkie okna itd
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Tu damy tytuï¿½:D", nullptr, nullptr);
     if (!window) {
-        cerr << "Nie uda³o siê utworzyæ okna GLFW\n";
-        glfwTerminate(); 
+        cerr << "Nie udaÅ‚o siÄ™ utworzyÄ‡ okna GLFW\n";
+        glfwTerminate();
         return -1;
     }
 
@@ -45,9 +70,60 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        cerr << "Nie uda³o siê za³adowaæ GLAD\n";
+        cerr << "Nie udaï¿½o siï¿½ zaï¿½adowaï¿½ GLAD\n";
         return -1;
     }
+
+    // Vertex shader i fragment shader (moÅ¼esz wstawiÄ‡ jako stringi)
+    const char* vertexSource = R"(
+    #version 330 core
+    layout (location = 0) in vec2 aPos;
+    void main() { gl_Position = vec4(aPos, 0.0, 1.0); }
+    )";
+
+    const char* fragmentSource = R"(
+    #version 330 core
+    out vec4 FragColor;
+    void main() { FragColor = vec4(0.3, 0.3, 0.9, 1.0); }
+    )";
+
+    GLuint vertexShader = CompileShader(GL_VERTEX_SHADER, vertexSource);
+    GLuint fragmentShader = CompileShader(GL_FRAGMENT_SHADER, fragmentSource);
+
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    // Sprawdzenie linkowania
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        cerr << "Shader program linking failed:\n" << infoLog << endl;
+    }
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+   
+
+    // VAO i VBO dla czÄ…stki
+    GLuint particleVAO, particleVBO;
+    glGenVertexArrays(1, &particleVAO);
+    glGenBuffers(1, &particleVBO);
+
+    glBindVertexArray(particleVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, particleVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 2, nullptr, GL_DYNAMIC_DRAW); // miejsce na 2 wspÃ³Å‚rzÄ™dne x,y
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glBindVertexArray(0);
+
+    // Ustaw rozmiar punktu
+    glPointSize(10.0f);
+
+
 
     // ImGui
     IMGUI_CHECKVERSION();
@@ -58,7 +134,7 @@ int main()
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
 
-    
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
 
@@ -67,18 +143,44 @@ int main()
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        ImGui::Begin("Witaj w ImGui!");
-        ImGui::Text("Gdzie elektron");
-        ImGui::Bullet(); //kropka
-        ImGui::Button("hej"); //przycisk
+        ImGui::Begin("Symulacja czÄ…stki");
+        ImGui::Text("ZmieÅ„ parametry");
+
+        ImGui::InputDouble("B[T] - natÄ™Å¼enie pola", &Bz);
+        ImGui::InputDouble("dt [s]", &dt);
+        ImGui::InputDouble("m[jednoskta] - masa czÄ…stki", &masa);
+
+        if (ImGui::Button("Start")) simulate = true;
+        ImGui::SameLine();
+        if (ImGui::Button("Stop")) simulate = false;
+        ImGui::SameLine();
+        if (ImGui::Button("Reset")) {
+            particle.Reset({ 0.0, 0.0 }, { 1.0, 0.0 });
+        }
         ImGui::End();
+        if (simulate) {
+            particle.UpdateRK4(dt, Bz, masa);
+        }
 
         ImGui::Render();
         int w, h;
         glfwGetFramebufferSize(window, &w, &h);
         glViewport(0, 0, w, h);
-        glClearColor(0.2f, 0.8f, 0.3f, 1.0f); //kolor t³a
+        glClearColor(0.8f, 0.8f, 0.8f, 1.0f); //kolor tÅ‚a
         glClear(GL_COLOR_BUFFER_BIT);
+        // Przygotuj dane do wysÅ‚ania do VBO
+       
+        float pos[2] = { (float)particle.position.x, (float)particle.position.y };
+        glBindBuffer(GL_ARRAY_BUFFER, particleVBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(pos), pos);
+
+        // Rysuj czÄ…stkÄ™
+        glUseProgram(shaderProgram);
+        glBindVertexArray(particleVAO);
+        glDrawArrays(GL_POINTS, 0, 1);
+        glBindVertexArray(0);
+        glUseProgram(0);
+
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
@@ -92,3 +194,4 @@ int main()
 
     return 0;
 }
+
